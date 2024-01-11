086e6bd7a88e4c5b2ab490e19e17b770f61edb23
bdybgs <bondarev.mc.2018@gmail.com>
2024-01-11 17:26:00 +0300

    added halls html(view) and changes hall controller

--- src/main/java/com/example/springexample/controllers/HallController.java
+++ src/main/java/com/example/springexample/controllers/HallController.java
@@ -3,47 +3,54 @@ package com.example.springexample.controllers;
 import com.example.springexample.dto.HallDto;
 import com.example.springexample.services.HallService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@RestController
-@RequestMapping("/api/halls")
+@Controller
+@RequestMapping("/halls")
 @RequiredArgsConstructor
 public class HallController {
 
     private final HallService hallService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<HallDto> getHallById(@PathVariable Integer id) {
-        return ResponseEntity.ok(hallService.getById(id));
-    }
-
     @GetMapping
-    public ResponseEntity<Collection<HallDto>> getAllHalls() {
-        return ResponseEntity.ok(hallService.getAll());
+    public String getAllHalls(Model model) {
+        Collection<HallDto> hallDtos = hallService.getAll();
+        model.addAttribute("halls", hallDtos);
+        return "hall";
     }
 
     @PostMapping
-    public ResponseEntity<Void> createHall(@RequestBody HallDto hallDto) {
+    public String createHall(@ModelAttribute HallDto hallDto) {
         hallService.create(hallDto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return "redirect:/halls";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateHall(@PathVariable Integer id, @RequestBody HallDto hallDto) {
-        hallDto.setId(Long.valueOf(id));
+    @PostMapping("/delete/{id}")
+    public String deleteHall(@PathVariable Long id) {
+        hallService.delete(Math.toIntExact(id));
+        return "redirect:/halls";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String getHallById(@PathVariable Long id, Model model) {
+        HallDto hallDto = hallService.getById(Math.toIntExact(id));
+        model.addAttribute("hall", hallDto);
+        return "edit-hall";
+    }
+
+    @PutMapping("/edit/{id}")
+    public String updateHall(@PathVariable Long id, @ModelAttribute HallDto hallDto) {
+        hallDto.setId(id);
         hallService.update(hallDto);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return "redirect:/halls";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteHall(@PathVariable Integer id) {
-        hallService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+    public String handleFormSubmit(@PathVariable Long id, @ModelAttribute HallDto hallDto) {
+        return updateHall(id, hallDto);
     }
 }
-
-
